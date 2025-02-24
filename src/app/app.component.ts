a0523ef798586877c255af4b6294048785642fbd
Hafid DNA <hafiddna@gmail.com>
2025-02-24 10:56:52 +0700

    Save Progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AuthService } from "./services/auth.service";
-import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
-  constructor(
-    private authService: AuthService
-  ) {}
-
+export class AppComponent {
   title = 'Auth Starterkit Angular Admin Template';
-
-  ngOnInit() {
-    this.authService.getProfile().pipe(
-      tap((profile) => {
-        console.log('Profile:', profile);
-      })
-    )
-  }
 }
