5223c738770d8b4cfac2d6c485fc7e596be859a9
Hafid DNA <hafiddna@gmail.com>
2025-02-25 11:12:19 +0700

    Save Progress

--- src/app/pages/authentication/authentication.routes.ts
+++ src/app/pages/authentication/authentication.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppChangePasswordComponent } from "./change-password/change-password.component";
 import { AppErrorComponent } from './error/error.component';
 import { AppForgotPasswordComponent } from "./forgot-password/forgot-password.component";
 import { AppSideLoginComponent } from './side-login/side-login.component';
 import { AppSideRegisterComponent } from './side-register/side-register.component';
+import { AppVerifyEmailComponent } from "./verify-email/verify-email.component";
 
 export const AuthenticationRoutes: Routes = [
   {
@@ -24,6 +26,14 @@ export const AuthenticationRoutes: Routes = [
       {
         path: 'forgot-password',
         component: AppForgotPasswordComponent
+      },
+      {
+        path: 'chage-password/:token',
+        component: AppChangePasswordComponent
+      },
+      {
+        path: 'verify-email/:token',
+        component: AppVerifyEmailComponent
       },
     ],
   },
