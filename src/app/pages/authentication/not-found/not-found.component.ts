c977cb30c09684f38f33f60751e8155e757aa686
Hafid Dian Nurfaujan Ahat <hafiddna@gmail.com>
2025-03-13 23:35:16 +0700

    Save Progress

--- src/app/pages/authentication/not-found/not-found.component.ts
+++ src/app/pages/authentication/not-found/not-found.component.ts
@@ -4,8 +4,7 @@ import { RouterModule } from "@angular/router";
 @Component({
   selector: 'app-not-found',
   imports: [RouterModule],
-  templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss'
+  templateUrl: './not-found.component.html'
 })
 export class NotFoundComponent {
 
